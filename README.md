e11384164f68ead260dc17fbf18876af768d8421
F. Christian Castro <f.christian.castro@gmail.com>
2020-11-03 23:43:43 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-admin-configuration
+Spring Boot Admin is a web application, used for managing and monitoring Spring Boot applications.
---
+++ src/main/java/com/example/springadminconfiguration/SpringAdminConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadminconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminConfigurationApplication.class, args);
+    }
+
+}
