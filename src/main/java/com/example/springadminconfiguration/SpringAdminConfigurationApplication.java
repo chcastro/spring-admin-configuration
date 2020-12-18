ce4e1c1ffd4ef4331321b9bde8ad600a73adf825
F. Christian Castro <f.christian.castro@gmail.com>
2020-12-17 23:49:29 -0600

    Configure server admin

--- src/main/java/com/example/springadminconfiguration/SpringAdminConfigurationApplication.java
+++ src/main/java/com/example/springadminconfiguration/SpringAdminConfigurationApplication.java
@@ -1,13 +1,14 @@
 package com.example.springadminconfiguration;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class SpringAdminConfigurationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringAdminConfigurationApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAdminConfigurationApplication.class, args);
+  }
 }
